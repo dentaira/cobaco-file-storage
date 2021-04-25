ec98e33d7305bb97b42ce4fc2d1c356614a7f620
dentaira <heismanreport.05@gmail.com>
2021-04-25 15:44:38 +0900

    Initial commit server-spring

---
+++ cobaco-server-spring/src/main/java/dentaira/cobaco/server/CobacoServerSpringApplication.java
@@ -0,0 +1,13 @@
+package dentaira.cobaco.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobacoServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobacoServerSpringApplication.class, args);
+    }
+
+}
