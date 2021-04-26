96ad059844749fa88a2525bc46e1434f27a17a05
dentaira <heismanreport.05@gmail.com>
2021-04-27 04:06:58 +0900

    update FolderRestController
    getRootFolder メソッドを追加。

--- cobaco-server-spring/src/main/java/dentaira/cobaco/server/file/web/api/FileRestController.java
+++ cobaco-server-spring/src/main/java/dentaira/cobaco/server/file/web/api/FileRestController.java
@@ -1,5 +1,7 @@
 package dentaira.cobaco.server.file.web.api;
 
+import dentaira.cobaco.server.file.DataSize;
+import dentaira.cobaco.server.file.FileType;
 import dentaira.cobaco.server.file.Owner;
 import dentaira.cobaco.server.file.StoredFile;
 import dentaira.cobaco.server.file.app.FileService;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -20,9 +23,20 @@ public class FileRestController {
         this.fileService = fileService;
     }
 
-    @GetMapping("api/folder/{fileId}/{userId}")
-    public FolderResource getFolder(@PathVariable String fileId, @PathVariable String userId) {
-        Owner owner = new Owner(UUID.fromString(userId));
+    @GetMapping("api/root/{ownerId}")
+    public FolderResource getRootFolder(@PathVariable String ownerId) {
+        Owner owner = new Owner(UUID.fromString(ownerId));
+
+        List<StoredFile> children = fileService.searchRoot(owner);
+        StoredFile root = new StoredFile(UUID.randomUUID(), "home", Path.of("/"), FileType.DIRECTORY, DataSize.of(0L));
+
+        List<FileResource> fileResources = children.stream().map(FileResource::of).collect(Collectors.toList());
+        return FolderResource.of(root, fileResources);
+    }
+
+    @GetMapping("api/folder/{fileId}/{ownerId}")
+    public FolderResource getFolder(@PathVariable String fileId, @PathVariable String ownerId) {
+        Owner owner = new Owner(UUID.fromString(ownerId));
 
         StoredFile folder = fileService.findById(fileId, owner);
         List<StoredFile> children = fileService.search(fileId, owner);
