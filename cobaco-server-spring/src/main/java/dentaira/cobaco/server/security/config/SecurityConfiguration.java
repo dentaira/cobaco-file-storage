cd7f6a2042acb89c9a10a39b031fa0163a0e5704
dentaira <heismanreport.05@gmail.com>
2021-04-28 04:38:01 +0900

    Disable CSRF

--- cobaco-server-spring/src/main/java/dentaira/cobaco/server/security/config/SecurityConfiguration.java
+++ cobaco-server-spring/src/main/java/dentaira/cobaco/server/security/config/SecurityConfiguration.java
@@ -3,11 +3,13 @@ package dentaira.cobaco.server.security.config;
 import dentaira.cobaco.server.security.user.UserAccountRepository;
 import dentaira.cobaco.server.security.user.spring.AccountUserDetailsService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -21,9 +23,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**/api/**").authorizeRequests().anyRequest().authenticated()
+        http.csrf().disable()
+                .antMatcher("/**/api/**").authorizeRequests().anyRequest().authenticated()
                 .and().httpBasic()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().logout().logoutUrl("/api/sign-out").logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.UNAUTHORIZED));
     }
 
     @Override
